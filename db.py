49b25774c47069a57d1ab8dacc2a09bb1b475e3c
Sneha14-c <2023cs_snehac_d@nie.ac.in>
2024-09-11 16:13:16 +0530

    test github

---
+++ db.py
@@ -0,0 +1,38 @@
+import mysql.connector
+
+connection=mysql.connector.connect(
+    host='localhost',
+    database='sdp',
+    user='root',
+    password='root123'
+)
+# step 3:check if the connections is successful
+
+if connection.is_connected():
+    print("successfully connected to the database")
+
+    cursor=connection.cursor()
+    create_table_query="""
+             CREATE TABLE IF NOT EXISTS students (
+                 id INT AUTO_INCREMENT PRIMARY KEY,
+                 name VARCHAR(225) NOT NULL,
+                 age INT,
+                 gender VARCHAR(10)
+                 )
+                 """
+    
+    cursor.execute(create_table_query)
+    print("table 'students' created successfully.")
+       
+    insert_query="""
+              INSERT INTO students (name,age,gender)
+              VALUES(%s, %s, %s)
+              """
+    students_records=[
+                  ('Alice',22,'female'),
+                  ('bob',24,'male'),
+                  ('charlie',23,'male')
+             ]
+    cursor.executemany(insert_query,students_records) 
+    connection.commit()
+    print(f"{cursor.rowcount} records inserted into 'students' table")
